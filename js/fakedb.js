cb1dd1bdbf46ea883bb7439b4d8f57e242460aa7
Arthur Gramiscelli Branco <arthurgbranco@gmail.com>
2018-10-20 11:07:08 -0300

    Versao GMaps API

---
+++ js/fakedb.js
@@ -0,0 +1,78 @@
+var dados = {
+    "results": {
+        "total_records": 5,
+        "data": [
+            {
+                "id": 1,
+                "toilettype": "Químico",
+                "locationtype": "Parque Ecologico",
+                "address": {
+                    "street": "American Drive",
+                    "number": "1621",
+                    "city": "Tallahassee"
+
+                },
+                "coordinates": {
+                    "lat": -19.932483,
+                    "lng": -43.938079
+                }
+            },
+            {
+                "id": 2,
+                "toilettype": "Acessível",
+                "locationtype": "McDonald's",
+                "address": {
+                    "street": "Kulas Light",
+                    "number": "556",
+                    "city": "Gwenborough"
+                },
+                "coordinates": {
+                    "lat": -19.932484,
+                    "lng": -43.938080
+                }
+            },
+            {
+                "id": 3,
+                "toilettype": "Comum",
+                "locationtype": "Shopping",
+                "address": {
+                    "street": "Harry Place",
+                    "number": "4108",
+                    "city": "Concord"
+                },
+                "coordinates": {
+                    "lat": -19.932485,
+                    "lng": -43.938081
+                }
+            },
+            {
+                "id": 4,
+                "toilettype": "Comum",
+                "locationtype": "Café",
+                "address": {
+                    "street": "Jett Lane",
+                    "number": "2497",
+                    "city": "Irvine"
+                },
+                "coordinates": {
+                    "lat": -19.932486,
+                    "lng": -43.938082
+                }
+            },
+            {
+                "id": 5,
+                "toilettype": "Acessível",
+                "locationtype": "Posto de Gasolina",
+                "address": {
+                    "street": "Penn Street",
+                    "number": "5648",
+                    "city": "Manchester"
+                },
+                "coordinates": {
+                    "lat": -19.932487,
+                    "lng": -43.938083
+                }
+            }
+        ]
+    }
+};
