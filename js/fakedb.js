c4501ee7dccde0f704b12eefd03f0b48e6d9d9b4
Arthur Gramiscelli Branco <arthurgbranco@gmail.com>
2018-11-07 23:24:55 -0200

    Fake DB FUNFANDO WOOO

--- js/fakedb.js
+++ js/fakedb.js
@@ -1,43 +1,37 @@
-var dados = {
-    "results": {
-        "total_records": 5,
-        "data": [
-            {
-                "id": 1,
-                "title": "Praça da Liberdade \nBanheiro publico\nLimpo.",
-                "longitude": "-43.938078",
-                "latitude": "-19.932482"
-            },
-            {
-                "id": 2,
-                "title": "Rua Ricardo Almeida\nBanheiro pago\nDentro do restaurante Santo Augusto\nAcesso para deficiente\nLimpo",
-                "longitude": "-43.937169",
-                "latitude": "-19.933168"
-            },
-            {
-                "id": 3,
-                "title": "Rua Marlene Penha\nBanheiro publico\nNa lanchonete Sinhazinha",
-                "longitude": "-43.941105",
-                "latitude": "-19.933869"
-            },
-            {
-                "id": 4,
-                "title": "Rua Alberto Magalhães\nBanheiro pago\nDentro da Lojas rede\nAcesso para deficiente\nLimpo",
-                "longitude": "-43.934410",
-                "latitude": "-19.934071"
-            },
-            {
-                "id": 5,
-                "title": "Rua pedro paulo\nBanheiro Quimico\nNa calçada",
-                "longitude": "-43.937682",
-                "latitude": "-19.927182"
-            },
-            {
-                "id": 6,
-                "title": "Rua 18 de julho\nBanheiro Pago\nDentro do Shopping BH",
-                "longitude": "-43.932951",
-                "latitude": "-19.927152"
-            }
-        ]
-    }
-};
+var json = [{
+        "id": 1,
+        "title": "Praça da Liberdade \nBanheiro publico\nLimpo.",
+        "longitude": "-43.938078",
+        "latitude": "-19.932482"
+                                },
+    {
+        "id": 2,
+        "title": "Rua Ricardo Almeida\nBanheiro pago\nDentro do restaurante Santo Augusto\nAcesso para deficiente\nLimpo",
+        "longitude": "-43.937169",
+        "latitude": "-19.933168"
+                                },
+    {
+        "id": 3,
+        "title": "Rua Marlene Penha\nBanheiro publico\nNa lanchonete Sinhazinha",
+        "longitude": "-43.941105",
+        "latitude": "-19.933869"
+                                },
+    {
+        "id": 4,
+        "title": "Rua Alberto Magalhães\nBanheiro pago\nDentro da Lojas rede\nAcesso para deficiente\nLimpo",
+        "longitude": "-43.934410",
+        "latitude": "-19.934071"
+                                },
+    {
+        "id": 5,
+        "title": "Rua pedro paulo\nBanheiro Quimico\nNa calçada",
+        "longitude": "-43.937682",
+        "latitude": "-19.927182"
+                                },
+    {
+        "id": 6,
+        "title": "Rua 18 de julho\nBanheiro Pago\nDentro do Shopping BH",
+        "longitude": "-43.932951",
+        "latitude": "-19.927152"
+                                }
+                            ];
