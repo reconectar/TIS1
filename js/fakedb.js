89faf2a05ab2a2efdb006fa4f0ceae1796809d00
Arthur Gramiscelli Branco <arthurgbranco@gmail.com>
2018-12-04 14:43:46 -0200

    various fix 2

--- js/fakedb.js
+++ js/fakedb.js
@@ -1,7 +1,4 @@
-
-var db = {
-    "data":  [
-        {
+var json = [{
         "id": 1,
         "title": "Praça da Liberdade",
         "tipo": "publico",
@@ -56,67 +53,3 @@ var db = {
         "latitude": "-19.927152"
                                 }
                             ];
-
-// Caso exista no Local Storage, recupera os dados salvos
-var db = JSON.parse(localStorage.getItem('db'));
-if (!db) {
-    db = dbFake
-};
-
-// Exibe mensagem em um elemento de ID msg
-function displayMessage(msg) {
-    $('.msg').html('<div class="alert alert-warning">' + msg + '</div>');
-}
-
-function insertContato(contato) {
-    // Calcula novo Id a partir do último código existente no array
-    let novoId = db.data[db.data.length - 1].id;
-    ++novoId
-    let novobanheiro = {
-
-        "id": novoId,
-        "title": contato.title,
-        "tipo": contato.tipo,
-        "especial": contato.especial,
-        "pago": contato.pago,
-        "longitude": contato.longitude,
-        "latitude": contato.latitude
-    };
-
-    // Insere o novo objeto no array
-    db.data.push(novobanheiro);
-    displayMessage("Contato inserido com sucesso");
-
-    // Atualiza os dados no Local Storage
-    localStorage.setItem('db', JSON.stringify(db));
-}
-
-function updateContato(id, contato) {
-    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
-    let index = id-1;
-
-    // Altera os dados do objeto no array
-       
-        db.data[index].title = contato.title,
-        db.data[index].tipo = contato.tipo
-        db.data[index].especial = contato.especial,
-        db.data[index].pago = contato.pago,
-        db.data[index].foto = contato.foto,
-        db.data[index].longitude = contato.longitude,
-        db.data[index].latitude = contato.latitude,
-
-    displayMessage("Contato alterado com sucesso");
-
-    // Atualiza os dados no Local Storage
-    localStorage.setItem('db', JSON.stringify(db));
-}
-
-function deleteContato(id) {    
-    // Filtra o array removendo o elemento com o id passado
-    db.data = db.data.filter(function (element) { return element.id != id });
-
-    displayMessage("Contato removido com sucesso");
-
-    // Atualiza os dados no Local Storage
-    localStorage.setItem('db', JSON.stringify(db));
-}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,68 @@
 /* Insira seu código em javascript abaixo */
 
 console.log('Olá TIS!');
+
+// Caso exista no Local Storage, recupera os dados salvos
+var dbb = JSON.parse(localStorage.getItem('db'));
+if (!dbb) {
+    dbb = json;
+};
+
+// Exibe mensagem em um elemento de ID msg
+function displayMessage(msg) {
+    $('.msg').html('<div class="alert alert-warning">' + msg + '</div>');
+}
+
+function insertContato(contato) {
+    // Calcula novo Id a partir do último código existente no array
+    let novoId = dbb[dbb.length - 1].id;
+    ++novoId
+    let novobanheiro = {
+
+        "id": novoId,
+        "title": contato.title,
+        "tipo": contato.tipo,
+        "especial": contato.especial,
+        "pago": contato.pago,
+        "longitude": contato.longitude,
+        "latitude": contato.latitude
+    };
+
+    // Insere o novo objeto no array
+    dbb.push(novobanheiro);
+    displayMessage("Banheiro inserido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db', JSON.stringify(dbb));
+}
+
+function updateContato(id, contato) {
+    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+    let index = id - 1;
+
+    // Altera os dados do objeto no array
+
+    dbb[index].title = contato.title,
+        dbb[index].tipo = contato.tipo
+    dbb[index].especial = contato.especial,
+        dbb[index].pago = contato.pago,
+        dbb[index].longitude = contato.longitude,
+        dbb[index].latitude = contato.latitude,
+
+        displayMessage("Contato alterado com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db', JSON.stringify(dbb));
+}
+
+function deleteContato(id) {
+    // Filtra o array removendo o elemento com o id passado
+    dbb = dbb.filter(function (element) {
+        return element.id != id
+    });
+
+    displayMessage("Banheiro removido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db', JSON.stringify(dbb));
+}
